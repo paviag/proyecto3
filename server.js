cc63b5cebba582c1af9a5fd8ca53a56f566e8d68
paviag <98894987+paviag@users.noreply.github.com>
2024-05-20 16:44:51 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 8088;
+const HOST = 'localhost';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.sendFile(__dirname+"/index.html")
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
